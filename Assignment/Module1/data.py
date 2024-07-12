4b04bd0b031bdee6f44208165f1bfd49e99e679e
Radhi18-dance <radhikapithadia123@gmail.com>
2024-07-12 22:24:41 +0530

    first commit

---
+++ Assignment/Module1/data.py
@@ -0,0 +1,13 @@
+name=input('Enter  name :')
+age=int(input('Enter age :'))
+DOB=int(input('Enter  date of birth :'))
+City=input('I m from:')
+Address=input('Address is:')
+
+
+print('\n\n OUTPUT')
+print("Name is:",name)
+print("Age is:",age)
+print("Birthdate is:",DOB)
+print("City is:",City)
+print("Resident  is:",Address)
