5510654bcf59dd8da7c5462c7da016eec7ec1eb5
Radhi18-dance <radhikapithadia123@gmail.com>
2024-07-25 17:14:29 +0530

    n

---
+++ Assignment/Module2/sum_of_three_integer.py
@@ -0,0 +1,9 @@
+no1=int(input("enter no1:"))
+no2=int(input("enter no2:"))
+no3=int(input("enter no3:"))
+result=0
+if no1==no2 or no2==no3 or no1==no3:
+    print(result)
+else:
+    result=no1+no2+no3
+    print("sum is:",result)
---
+++ Assignment/Module2/sumofnpositivenumbers.py
@@ -0,0 +1,6 @@
+summation=0
+result=int(input("enter any positive no:"))
+for i in range(1,result+1):
+    if result>0:
+        summation+=i
+print("sum is:",summation)
