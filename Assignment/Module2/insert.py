15c8c9c506bc51a72f5cf0b3b257edeee1818503
Radhi18-dance <radhikapithadia123@gmail.com>
2024-08-01 18:13:39 +0530

    l

--- Assignment/Module2/insert.py
+++ Assignment/Module2/insert.py
@@ -1,13 +1,9 @@
 str=input("enter any string:")
 new=input("enter any new string:")
-x=print(str.split())
-print(len(str))
-if len>=5:
- print(input("enter position:"))
 
- newstr=str[:1]+new[1:]
-else:
- print(str)
+position=len(str)
+if position%2:
+
+ print(str[:position]+new+str[position:])
 
 
-print(newstr)
---
+++ Assignment/Module2/string_made.py
@@ -0,0 +1,4 @@
+str=input("enter string:")
+#for i in range(str):
+newstr= "{}{}".format(str[0:2],str[-2:])
+print(newstr)
