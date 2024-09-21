6c1250a80e966c65197c2a7bab3132f29e137a2d
Radhi18-dance <radhikapithadia123@gmail.com>
2024-09-21 17:17:29 +0530

    tu

---
+++ Assignment/Module3/create_tuple.py
@@ -0,0 +1,5 @@
+string=input("enter string value:")
+int_tuple=int(input("enter int tuple:"))
+my_tuple=(string,int_tuple)
+
+print("tupple with different data types:",my_tuple)
---
+++ Assignment/Module3/list_into_variables.py
@@ -0,0 +1,14 @@
+colour=[]
+var=''
+n=int(input("enter size:"))
+for i in range(n):
+    val=input("enter values:")
+    colour.append(val)
+     
+for  i in colour:
+    var+='val'
+    var=colour
+
+print(var)
+ 
+  
