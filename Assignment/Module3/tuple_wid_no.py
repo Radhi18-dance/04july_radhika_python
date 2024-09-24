9ee0443f629f3d373c6f2c13ba1487a824185e25
Radhi18-dance <radhikapithadia123@gmail.com>
2024-09-24 16:47:32 +0530

    new

--- Assignment/Module3/tuple_wid_no.py
+++ Assignment/Module3/tuple_wid_no.py
@@ -1,6 +1,6 @@
 my_tuple=[]
 n=int(input("enter size:"))
 for i in range(n):
-    val=input("enter values:")
+    val=int(input("enter values:"))
     my_tuple.append(val)
 print("tuple with numbers is:",tuple(my_tuple))
