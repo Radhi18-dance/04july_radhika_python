9a5c5b3c188c0d687214753f88e1ce511acd8935
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-01 16:59:57 +0530

    ki

--- Assignment/Module3/remove_empty_tuple.py
+++ Assignment/Module3/remove_empty_tuple.py
@@ -1,15 +1,16 @@
+myli=[]
 empty_tuple = [] 
-n1 = int(input("How many tuple do you want to enter? : "))
+n= int(input("How many tuple do you want to enter? : "))
 
-for i in range(n1):
-    val=input("enter values for tupple:")
-    empty_tuple.append(val)
-
-
-print("Original List:",tuple(empty_tuple))
+for i in range(n):
+    val=input(f"enter{i+1}values for tupple:")
+    myli.append(tuple(val.split()))
 
 
-for i in empty_tuple:
-     if len(i) == 0:
-        empty_tuple.remove(i)
+print("Original List:",myli)
+
+
+for j in myli:
+     if j:
+        empty_tuple.append(j)
 print(f'Removing Empty tuple from tuple_list: {empty_tuple}')
