43bf8ed8e856a8ada8108997b54a8723b843a664
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-04 17:35:30 +0530

    d

--- Assignment/Module3/unzip.py
+++ Assignment/Module3/unzip.py
@@ -1,15 +1,15 @@
-myli=[]
-myli2=[]
 n=int(input("enter size:"))
+l=int(input("enter length:"))
+myli=[]
 for i in range(n):
-   val=input(f"enter{i+1} values:")
-   myli.append(tuple(val.split()))
-   print(myli)
-n2=int(input("enter size:"))
-for i in range(n2):
-   val2=input(f"enter{i+1} values:")
-   myli2.append(tuple(val2.split()))    
-   print(myli2)
-res=list(zip(*myli,*myli2))
-print("modified list is:",res)
+   mylist2=[]
+   for j in range(l):
+      val=input("enter values:")
+      mylist2.append(val)
+   myli.append(tuple(mylist2))
+  
+res=list(zip(*myli))
+print(res)
+for i in res:
+   print(list(i))
 
