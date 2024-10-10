2f3f2c13ef2a55eb24f6f0636e93a8150bba43e1
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-10 16:54:32 +0530

    f

---
+++ Assignment/Module3/print_uniquevalues_indic.py
@@ -0,0 +1,15 @@
+d1={}
+d2=[]
+n1= int(input("Enter the number of key-value pairs: "))
+for i in range(n1):
+    key = input("Enter key : ".format(i+1))
+    value = input("Enter value for key : ".format(key))
+    d1[key] = value
+print(d1)
+for i,j in d1.items():
+    if j not in d2:
+        d2.append(j)
+
+print(f'Dict: {d1}\n')
+
+print('Unique Value in Dictionary: ',d2)
