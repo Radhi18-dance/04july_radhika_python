16a164f370b8b6d7976c0a1330f48190a2f15208
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-12 16:50:53 +0530

    tr

---
+++ Assignment/Module3/area_of_trapezoid.py
@@ -0,0 +1,7 @@
+top=int(input('Enter Value of Top: '))
+bottom=int(input('Enter Value of Bottom: '))
+height=int(input('Enter Value of Height: '))
+trapezoid=((top+bottom)/2)*height
+
+print(f'Area of a Trapezoid: {trapezoid}')
+
