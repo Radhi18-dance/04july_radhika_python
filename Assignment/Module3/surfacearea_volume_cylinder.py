67681520c51584cb9b8f53400cfac5ea9038cf8e
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-12 16:56:49 +0530

    d

---
+++ Assignment/Module3/surfacearea_volume_cylinder.py
@@ -0,0 +1,6 @@
+radius=int(input('Enter Value of Radius: '))
+height=int(input('Enter Value of Height: '))
+area=2*3.14*(radius*radius) + 2*3.14*radius*height
+volume=3.14*(radius*radius)*height
+print("surface area :",area)
+print("surface volume :",volume)
