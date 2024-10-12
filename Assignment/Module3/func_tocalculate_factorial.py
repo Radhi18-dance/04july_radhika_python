3d6c06dfde18232177d6f9bedeef562395c90e7d
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-12 15:19:51 +0530

    dh

--- Assignment/Module3/func_tocalculate_factorial.py
+++ Assignment/Module3/func_tocalculate_factorial.py
@@ -3,5 +3,5 @@ def factorial():
     factorial=1
     for i in range(1,no+1):
         factorial=factorial*i
-    print(f'factorial of{no} is{factorial}')
+    print(f'factorial of {no} is {factorial}')
 factorial()
