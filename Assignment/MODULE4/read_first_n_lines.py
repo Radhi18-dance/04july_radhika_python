5760f4c6c280817c81abf2e34e833624c3e60af8
Radhi18-dance <radhikapithadia123@gmail.com>
2024-10-17 18:05:16 +0530

    rd

--- Assignment/MODULE4/read_first_n_lines.py
+++ Assignment/MODULE4/read_first_n_lines.py
@@ -1,3 +1,10 @@
-fl=open('stdata.txt','r')
-print(fl.readline())
-    
+
+file=open('stdata.txt','a')
+
+file=open('stdata.txt','r')
+
+n=int(input('How many lines You want to read?: '))
+
+for i in range(n):
+    print(file.readline())
+
---
+++ Assignment/MODULE4/read_last_lines.py
@@ -0,0 +1,5 @@
+fl=open('stdata.txt','r')
+n=5
+print(fl.readline()[-1:])
+if len('stdata.txt')>=n:
+    print('stdata.txt')
